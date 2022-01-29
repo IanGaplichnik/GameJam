d8e75c1998055dd002ef283cb296cabfb16fc16a
personalmac <ashatalo@student.hive.fi>
2022-01-29 19:24:55 +0200

    import to scripts

--- src/block.js
+++ src/block.js
@@ -4,11 +4,11 @@ class BlockUP {
 		this.height = 30;
 
 		this.speed = -7;
-
+		this.markedForDeletion = false;
 
 		this.position = {
-			x: gameWidth - this.width,
-			y: gameHeight/2 - this.height,
+			x: gameWidth,
+			y: gameHeight/2 - this.height + 2,
 	};
 }
 
@@ -18,6 +18,38 @@ class BlockUP {
 
 
 	draw(ctx){
+		ctx.fillStyle = 'green';
+		ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+	}
+
+	update(deltaTime) {
+		this.position.x += this.speed;
+
+		if (this.position.x <= - this.width) this.markedForDeletion = true;
+	}
+}
+
+/*class BlockBELOW {
+	constructor(gameWidth, gameHeight) {
+		this.width = 30;
+		this.height = 30;
+
+		this.speed = -5;
+
+
+		this.position = {
+			x: gameWidth - this.width,
+			y: gameHeight/2 - 2,
+	};
+}
+
+	stop() {
+		this.speed = 0;
+	}
+
+
+	draw(ctx){
+		ctx.fillStyle = 'red';
 		ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
 	}
 
@@ -27,3 +59,4 @@ class BlockUP {
 		if (this.position.x < 0) this.position.x = 0;
 	}
 }
+*/
--- src/index.js
+++ src/index.js
@@ -5,22 +5,40 @@ let ctx = canvas.getContext('2d');
 
 const GAME_WIDTH = 900;
 const GAME_HEIGHT = 600;
-
+let obstacles = [];
 let block = new BlockUP(GAME_WIDTH, GAME_HEIGHT);
+//let blockb = new BlockBELOW(GAME_WIDTH, GAME_HEIGHT);
+obstacles.push(block);
 let character = new Character(GAME_WIDTH, GAME_HEIGHT);
 new InputHandler(character);
 
 let lastTime = 0;
+let spawnTime = 0;
 function gameLoop(timestamp) {
 	let deltaTime = timestamp - lastTime;
 	lastTime = timestamp;
 
+
 	ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-	block.update(deltaTime);
+	if (timestamp - spawnTime >= 1000) {
+		spawnTime = timestamp;
+		obstacles.push(new BlockUP(GAME_WIDTH, GAME_HEIGHT));
+	}
+	obstacles.forEach(object => object.update(deltaTime));
+	obstacles = obstacles.filter(block => !block.markedForDeletion);
+	obstacles.forEach(object => object.draw(ctx));
+
+
+
+	/*
 	block.draw(ctx);
 
+	blockb.update(deltaTime);
+	blockb.draw(ctx);
+*/
 	character.update(deltaTime);
 	character.draw(ctx);
+
 
 	requestAnimationFrame(gameLoop);
 }
